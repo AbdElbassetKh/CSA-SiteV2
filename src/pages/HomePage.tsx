f4ff012c483aa7d01aad369cde3e2652a275ff03
Abd Elbasset Khettabi <abdelbassetkhettabi@gmail.com>
2024-11-10 02:15:15 +0100

    Add files via upload

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import FAQ from '../components/FAQ';
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FAQ />
+    </>
+  );
+}
