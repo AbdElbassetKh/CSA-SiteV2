1ebd098d2ef9e024c95dab7945677f92fb23bb69
Abd Elbasset Khettabi <abdelbassetkhettabi@gmail.com>
2024-11-10 02:21:20 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,30 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import WhyUs from './components/WhyUs';
-import Features from './components/Features';
-import FAQ from './components/FAQ';
-import Newsletter from './components/Newsletter';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import WhyUsPage from './pages/WhyUsPage';
+import ProgramDetailsPage from './pages/ProgramDetailsPage';
+import LoginPage from './pages/LoginPage';
+import QuestionPage from './pages/QuestionPage';
 import Footer from './components/Footer';
-import Login from './components/Login';
-import QuestionForm from './components/QuestionForm';
-import ProgramDetails from './components/ProgramDetails';
+import Newsletter from './components/Newsletter';
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-
-  const handleNavigation = (page: string) => {
-    setCurrentPage(page);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'login':
-        return <Login />;
-      case 'question':
-        return <QuestionForm />;
-      case 'program-details':
-        return (
-          <>
-            <ProgramDetails />
-            <Newsletter />
-            <Footer onNavigate={handleNavigation} />
-          </>
-        );
-      case 'about':
-        return (
-          <>
-            <About />
-            <Newsletter />
-            <Footer onNavigate={handleNavigation} />
-          </>
-        );
-      case 'why-us':
-        return (
-          <>
-            <WhyUs />
-            <Newsletter />
-            <Footer onNavigate={handleNavigation} />
-          </>
-        );
-      default:
-        return (
-          <>
-            <Hero onNavigate={handleNavigation} />
-            <Features onNavigate={handleNavigation} />
-            <FAQ onNavigate={handleNavigation} />
-            <Newsletter />
-            <Footer onNavigate={handleNavigation} />
-          </>
-        );
-    }
-  };
-
   return (
-    <div className="bg-black min-h-screen">
-      <Navbar onNavigate={handleNavigation} currentPage={currentPage} />
-      {renderPage()}
-    </div>
+    <Router>
+      <div className="bg-black min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/why-us" element={<WhyUsPage />} />
+          <Route path="/program-details" element={<ProgramDetailsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/question" element={<QuestionPage />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </div>
+    </Router>
   );
 }
